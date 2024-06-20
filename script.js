9f757e03e80d4eac9bb27e45da4204239cd0308b
jackdener <89590497+jackdener@users.noreply.github.com>
2024-06-19 22:10:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const campoSenha = document.getElementById("password");
 const campoNovoLogin = document.getElementById("newusername");
 const campoNovaSenha = document.getElementById("newpassword");
 const campoRepSenha = document.getElementById("reppassword");
+const campoTitulo = document.getElementById("titulo");
+const campoAutor = document.getElementById("autor");
+const campoGenero = document.getElementById("genero");
+const campoIsbn = document.getElementById("isbn");
+const lista = document.getElementById("lista");
 
 function logar() {
   let login = campoLogin.value;
@@ -13,10 +18,10 @@ function logar() {
     mensagem = "Nenhum usuário cadastrado até o momento";
   } else {
     for (let usuario of bancoDeDados) {
-      if (usuario.login == login && usuario.senha == senha) {
+      if (usuario.login === login && usuario.senha === senha) {
         mensagem = "Parabéns, você logou!";
         localStorage.setItem("logado", JSON.stringify(usuario));
-        window.location.href = 'home.html';
+        window.location.href = "home.html";
         break;
       }
     }
@@ -34,14 +39,73 @@ function cadastrar() {
     if (bancoDeDados == null) {
       bancoDeDados = [];
     }
-    bancoDeDados.push(usuario);
-    localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
-    alert("Usuário cadastrado com sucesso!");
+    if (existe(usuario, bancoDeDados)) {
+      alert("Esse login já foi cadastrado anteriormente");
+    } else {
+      bancoDeDados.push(usuario);
+      localStorage.setItem("bancoDeDados", JSON.stringify(bancoDeDados));
+      alert("Usuário cadastrado com sucesso!");
+    }
   } else {
     alert("As senhas são diferentes!");
   }
 }
 
-function logout(){
-    window.location.href = 'index.html';
+function existe(usuario, bancoDeDados) {
+  for (let verificado of bancoDeDados) {
+    if (verificado.login == usuario.login) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function logout() {
+  window.location.href = "index.html";
+}
+
+function criaLivro() {
+  const livro = {
+    titulo: campoTitulo.value,
+    autor: campoAutor.value,
+    genero: campoGenero.value,
+    isbn: campoIsbn.value,
+  };
+  let biblioteca = JSON.parse(localStorage.getItem("biblioteca"));
+  if (biblioteca == null) {
+    biblioteca = [];
+  }
+  //verificar aqui se o ISBN já existe, só permitindo cadastro caso não exista
+  biblioteca.push(livro);
+  localStorage.setItem("biblioteca", JSON.stringify(biblioteca));
+  alert("Livro cadastrado com sucesso!")
+  campoTitulo.value = null;
+  campoAutor.value = null;
+  campoGenero.value = null;
+  campoIsbn.value = null;
+}
+
+let aberto = false;
+function exibe() {
+  let livros = "";  
+  if (!aberto) {
+    aberto = true;
+    let biblioteca = JSON.parse(localStorage.getItem("biblioteca"));
+    if (biblioteca == null) {
+      livros = "Não há livros cadastrados no momento!";
+    } else {
+      for (let livro of biblioteca) {
+        livros += "<br><strong>Título: </strong>" + livro.titulo;
+        livros += "<br><strong>Autor: </strong>" + livro.autor;
+        livros += "<br><strong>Gênero: </strong>" + livro.genero;
+        livros += "<br><strong>ISBN: </strong>" + livro.isbn;
+        livros += "<br><strong>__________________________</strong>";
+      }
+      lista.innerHTML = livros;
+    }
+  }
+  else{
+    aberto = false;
+    lista.innerHTML = "";
+  }
 }
